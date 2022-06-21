29508315610117c1541235db640ab6126914a82a
unknown <dalun0419@gmail.com>
2022-06-21 03:24:46 -0700

    try and catch

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -12,29 +12,55 @@ class ProductCategoryController extends Controller
 
     public function index()
     {
-        return ProductCategoryResource::collection(ProductCategory::all());
+        try {
+            return ProductCategoryResource::collection(ProductCategory::all());
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+        
     }
 
     public function store(StoreProductCategoryRequest $request)
     {
-        $category = ProductCategory::create($request->all());
-        return new ProductCategoryResource($category);
+        try {
+            $category = ProductCategory::create($request->all());
+            return new ProductCategoryResource($category);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+
     }
 
     public function show(ProductCategory $productCategory)
     {
-        return new ProductCategoryResource($productCategory);
+        try {
+            return new ProductCategoryResource($productCategory);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+        
     }
 
     public function update(UpdateProductCategoryRequest $request, ProductCategory $productCategory)
     {
-        $category = $productCategory->update($request->all());
-        return new ProductCategoryResource($category);
+        try {
+            $category = $productCategory->update($request->all());
+            return new ProductCategoryResource($category);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+        
     }
 
     public function destroy(ProductCategory $productCategory)
     {
-        $category = $productCategory->delete();
-        return response()->json(['message' => 'Category deleted successfully'], 204);
+        try {
+            $category = $productCategory->delete();
+            return response()->json(['message' => 'Category deleted successfully'], 204);
+            
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+        
     }
 }
